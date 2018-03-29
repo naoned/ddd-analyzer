c6138acce537161f42d781a5b39793c8507736a8
Romain RENAUD <devwebpeanuts@gmail.com>
2018-03-29 14:32:42 +0200

    feat: display graph

--- assets/js/Viewer/index.js
+++ assets/js/Viewer/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 //FIX ME : remove report.json and make webservice call to get report content 
 import report from './report.json'
 import { options } from './configuration/graph'
 import { Viewer } from './Components/Viewer.jsx'
 
-ReactDOM.render(<Viewer options={options} report={report} /> , document.getElementById('viewer'))
+ReactDOM.render(
+    <Viewer
+        options={options}
+        report={report} 
+    />
+, document.getElementById('viewer'))
